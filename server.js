5a799fcf738bfae189fcc80d104e9d3968624b57
Conal Patrick Cosgrove <cosgroco@tcd.ie>
2018-04-10 15:43:57 +0100

    Removed unnecessary code

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(process.env.PORT ||8000, function() {
 })
