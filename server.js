3fb202da3c3a2be7bd0bd6fc75bd8c98ffac1b8a
Conal Patrick Cosgrove <cosgroco@tcd.ie>
2018-04-10 02:21:08 +0100

    Removed unnecessaryheader addition

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(process.env.PORT ||8000, function() {
-  
 })
 
 app.get('/',(request,response)=>{
 	response.sendFile(__dirname + "/index.html");
 })
+
 app.get('/scoreJSON', (request,response)=>{
 	var data = {};
 	axios.get('http://www.nfl.com/liveupdate/scores/scores.json').then((resp) => {
-		response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 		response.setHeader('Access-Control-Allow-Origin', '*');
 		response.send(resp.data);
 	});
