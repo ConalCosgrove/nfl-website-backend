272b6d87669c5130db9c1bc96ce785ab120f8806
Conal Patrick Cosgrove <cosgroco@tcd.ie>
2018-04-13 14:41:53 +0100

    Added random NFL json query

--- server.js
+++ server.js
@@ -21,3 +21,54 @@ app.get('/scoreJSON', (request,response)=>{
 	});
 })
 
+app.get('/fakescoreJSON',(request,response)=>{
+	var data = 					{ "0":{
+                                      "home":{"abbr":"PHI","score":{"T":"51"}},
+                                      "away":{"abbr":"GB","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "1":{
+                                      "home":{"abbr":"LA","score":{"T":"51"}},
+                                      "away":{"abbr":"TEN","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "2":{
+                                      "home":{"abbr":"JAX","score":{"T":"51"}},
+                                      "away":{"abbr":"LAC","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "3":{
+                                      "home":{"abbr":"CIN","score":{"T":"51"}},
+                                      "away":{"abbr":"KC","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "4":{
+                                      "home":{"abbr":"CLE","score":{"T":"51"}},
+                                      "away":{"abbr":"IND","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "5":{
+                                      "home":{"abbr":"NYG","score":{"T":"51"}},
+                                      "away":{"abbr":"PIT","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "6":{
+                                      "home":{"abbr":"SF","score":{"T":"51"}},
+                                      "away":{"abbr":"NYJ","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "7":{
+                                      "home":{"abbr":"HOU","score":{"T":"51"}},
+                                      "away":{"abbr":"MIA","score":{"T":"10"}},
+                                      "qtr":"Final","clock":"13:12"
+                                      },
+                                  "8":{
+                                      "home":{"abbr":"MIN","score":{"T":"29"}},
+                                      "away":{"abbr":"NO","score":{"T":"24"}},
+                                      "qtr":"4","clock":"00:00"
+                                      }
+                                  };
+	response.setHeader('Access-Control-Allow-Origin', '*');
+	response.send();
+})
+
