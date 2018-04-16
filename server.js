aa9a3f66d123107168ba50e30453a4c703a8175d
Conal Patrick Cosgrove <cosgroco@tcd.ie>
2018-04-16 02:08:39 +0100

    Added /images-2018 endpoint for use with test social media app

--- server.js
+++ server.js
@@ -72,3 +72,14 @@ app.get('/fakescoreJSON',(request,response)=>{
 	response.send(data);
 })
 
+app.get('/images-2018',(request,repsonse) => {
+  var data = {
+    "0":"https://scontent-lhr3-1.cdninstagram.com/vp/083a808a4b0d7edbdfa2f911cc42102d/5B540A36/t51.2885-15/s640x640/sh0.08/e35/27880379_539719523093485_7561558132430733312_n.jpg",
+    "1":"https://scontent-lhr3-1.cdninstagram.com/vp/2fc43cdcad8f0bf15b8d2bb4f95a2cb3/5B6B90D5/t51.2885-15/e35/23507829_1982430162015288_2608747334405718016_n.jpg",
+    "2":"https://scontent-lhr3-1.cdninstagram.com/vp/6e34c2d4b24077282581160393d6f9cf/5B4FF28D/t51.2885-15/e35/15251724_240986779652739_8479659167781486592_n.jpg",
+    "3":"https://scontent-lhr3-1.cdninstagram.com/vp/ac7f656dc4888258a0a54f21bbebea3f/5B4F3874/t51.2885-15/e35/14547838_1118909908192996_8241986436513923072_n.jpg"
+  }
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.send(data);
+})
+
